d7d7eb78a86f54e51b37ddd489e72d474944e976
Diljith <diljith@C-LM-0299.local>
2020-04-27 23:39:53 +0530

    Add the sqlite database

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 # Sri muniappan thunai
+from flask import Flask
+
+app = Flask(__name__)             
+
+
+
+@app.route("/")                   # at the end point /
+def hello():                      # call method hello
+    return "Parassini kadavu Muthappa kathalone"         # which returns "hello world"
+
+
+
+if __name__ == "__main__": 
+    Schema()       # on running python app.py
+    app.run(debug=True)                     # run the flask app
---
+++ models.py
@@ -0,0 +1,34 @@
+import sqlite3
+class Schema:
+    def __init__(self):
+        self.conn = sqlite3.connect('todo.db')
+        self.create_user_table()
+        self.create_to_do_table()
+        # Why are we calling user table before to_do table
+        # what happens if we swap them?
+
+    def create_to_do_table(self):
+
+        query = """
+        CREATE TABLE IF NOT EXISTS "Todo" (
+          id INTEGER PRIMARY KEY,
+          Title TEXT,
+          Description TEXT,
+          _is_done boolean,
+          _is_deleted boolean,
+          CreatedOn Date DEFAULT CURRENT_DATE,
+          DueDate Date,
+          UserId INTEGER FOREIGNKEY REFERENCES User(_id)
+        );
+        """
+
+        self.conn.execute(query)
+    def create_user_table(self):
+       query = """
+       CREATE TABLE IF NOT EXISTS "Users" (
+           id INTEGER PRIMARY KEY,
+           name TEXT NOT NULL,
+           email TEXT NOT NULL UNIQUE
+       );
+       """
+       
